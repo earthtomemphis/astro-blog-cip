9a5768c9c28f070d95ed0fc797c860089a0c2503
Memphis DiAngelis <earthtomemphis@gmail.com>
2026-04-07 20:34:04 +0800

    added routes and md files

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,6 @@
 const navLinks=document.querySelectorAll('[data-navLink]')
 
-navLinks.forEach((link)=>{
+navLinks.forEach((link) => {
     if (link.getAttribute('href') === window.location.pathname) {
         link.setAttribute('aria-current','page')
     }
